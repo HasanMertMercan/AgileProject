a9b5f97117ad29d4e6270eccff80e3f66e9b70f4
ahmetsafasezgin <ahmetsafasezgin@gmail.com>
2016-12-18 00:13:57 +0200

    Update emailConfirmationLink.php

--- bookExchange/emailConfirmationLink.php
+++ bookExchange/emailConfirmationLink.php
@@ -1,6 +1,5 @@
 <?php
 include("config.php");
-
 if (empty($_GET["token"])) {
     echo 'Missing required information';
     return;
@@ -8,25 +7,25 @@ if (empty($_GET["token"])) {
 
 $token = htmlentities($_GET["token"]);
 
-
     $sql = "SELECT id FROM emailTokens WHERE token = '$token'";
     $result = mysqli_query($db,$sql);
     $row = mysqli_fetch_array($result);
+
 if(empty($row["id"])) {
 	echo 'User with this token is not found';
 	return;
 }
 
-	$sql2 = "UPDATE users SET emailConfirmed = '1' WHERE id = ".mysqli_insert_id($db)." LIMIT 1";
+else {
+
+    $sql2 = "UPDATE users SET emailConfirmed = '1' WHERE  '".$row['id']."'";
+
     $result2 = mysqli_query($db, $sql2);
 
-if ($result2) {
-
-	$sql3 = "DELETE * FROM emailTokens WHERE token= $token";
-    $result3 = mysqli_query($db, $sql2);
-
-if ($result3) {
-	echo 'Thank You! Your email is now confirmed';
-}
+    if($result2) {
+        
+    echo 'Thank You! Your email is now confirmed';        
+         
+    }
 }
 ?>
