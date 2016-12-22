0c024ae04be8d7c42baaf3fd70e085ea92b96552
ahmetsafasezgin <ahmetsafasezgin@gmail.com>
2016-12-22 20:58:22 +0200

    Update register.php

--- bookExchange/register.php
+++ bookExchange/register.php
@@ -62,7 +62,7 @@ if(isset($_POST['submit'])) {
 
               
             if(! $result ) {
-              die('Could not enter data: ' . mysql_error());    
+              $error .= "Unable to register!.<br>";
               
             }
 
@@ -199,6 +199,7 @@ if(isset($_POST['submit'])) {
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
             <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>     
+
 
   </body>
 </html>
