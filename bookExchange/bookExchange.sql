7162fdbe1ec2778c5fdc5f6845b0afa38718e8bd
ahmetsafasezgin <ahmetsafasezgin@gmail.com>
2016-12-22 20:50:49 +0200

    Add files via upload

--- bookExchange/bookExchange.sql
+++ bookExchange/bookExchange.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Anamakine: localhost
--- Üretim Zamanı: 18 Ara 2016, 00:27:02
+-- Üretim Zamanı: 22 Ara 2016, 20:52:02
 -- Sunucu sürümü: 10.1.13-MariaDB
 -- PHP Sürümü: 5.6.23
 
@@ -19,6 +19,30 @@ SET time_zone = "+00:00";
 --
 -- Veritabanı: `bookExchange`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `books`
+--
+
+CREATE TABLE `books` (
+  `bookId` int(11) NOT NULL,
+  `bookName` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `author` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `ownerId` int(11) NOT NULL,
+  `PublishYear` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Tablo döküm verisi `books`
+--
+
+INSERT INTO `books` (`bookId`, `bookName`, `author`, `ownerId`, `PublishYear`) VALUES
+(3, 'isim', 'yazar', 65, 2000),
+(11, '213123', '213213', 58, 2131),
+(12, 'sadqd', '213321', 58, 1231),
+(13, 'asdasd', 'sadsadqwewqe', 58, 1111);
 
 -- --------------------------------------------------------
 
@@ -42,7 +66,19 @@ INSERT INTO `emailTokens` (`id`, `token`) VALUES
 (61, '0SKU584Rvcx9OoTkgNGH'),
 (62, 'eHpUW0sdNIQzfBItxsdv'),
 (63, 'pb4uMbh8AL4SKLOx35Ar'),
-(64, 'qfaAvUZeuRqwTDih4gRo');
+(64, 'qfaAvUZeuRqwTDih4gRo'),
+(64, 'I6KeLDsumKRzMqAovml5'),
+(65, 'UoipZJmW47WuwVRj0C7S'),
+(66, 'm9C2K3UkGmiNnBmp3Ss5'),
+(67, 'v7lr2KfXAEwqVOoQURba'),
+(68, 'NzCzMAHxh6mK09oYCYq9'),
+(70, 'qORybxzNxJ0DEhYBTk0E'),
+(72, 'c6QrchqwdxQy3K6lyf86'),
+(73, 'M0HuTfw8FL87FRMg2Uyk'),
+(74, 'lva6IxzlXOZigS0bCfiA'),
+(75, 'zvObDVGMaNOBn1fz1aZq'),
+(76, 'wJrVzyhmQYLX34HQf0Z8'),
+(77, 'aCpAAJW8gFWFgBd8jkKC');
 
 -- --------------------------------------------------------
 
@@ -61,7 +97,27 @@ CREATE TABLE `passwordTokens` (
 
 INSERT INTO `passwordTokens` (`id`, `token`) VALUES
 (58, 'RY3c1V5t3yK00TcfjRVN'),
-(64, 'l2a0fjGrmpjsviL83Gsu');
+(64, 'l2a0fjGrmpjsviL83Gsu'),
+(58, 'vXxplnwcxxXCplVvgZ2S'),
+(68, 'kBh4zhCd8RlH16746Kuy'),
+(75, 'Ibij42pKBVNf1oJYRKxf'),
+(75, 'd8BdTM3BSkden9ltyGQs'),
+(75, 'gCRlTUz3518lFtNxxhjq'),
+(73, 'a4FTbzas5hpluNRql8zZ');
+
+-- --------------------------------------------------------
+
+--
+-- Görünüm yapısı durumu `searchview`
+--
+CREATE TABLE `searchview` (
+`bookName` varchar(100)
+,`author` varchar(100)
+,`PublishYear` int(11)
+,`fullname` varchar(50)
+,`email` varchar(50)
+,`phone` varchar(10)
+);
 
 -- --------------------------------------------------------
 
@@ -76,7 +132,7 @@ CREATE TABLE `users` (
   `salt` binary(20) NOT NULL,
   `fullname` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `phone` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
-  `emailConfirmed` int(1) NOT NULL,
+  `emailConfirmed` int(1) NOT NULL DEFAULT '0',
   `isActive` int(1) NOT NULL DEFAULT '1',
   `isAdmin` int(1) NOT NULL DEFAULT '0',
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
@@ -87,13 +143,32 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `email`, `password`, `salt`, `fullname`, `phone`, `emailConfirmed`, `isActive`, `isAdmin`, `date`) VALUES
-(58, 'ahmetsafasezgin@gmail.com', 'd3bcbb163d1c6e3e6a8a9dbb4fee543f6e351d77', 0x77b255ce0477ae70acb4ea46aa4d1885bc5741eb, 'ahmet safa sezgin', '5351231212', 1, 1, 0, '2016-12-17 15:32:31'),
-(62, 'ahmet@aktif.com', 'e59bf993ca931f7007ea4d18c37a3976ce7f4127', 0x188b9dd82d04d2e1007434ce374ea861fc1aa58d, 'ahmet_aktif', '1231231231', 1, 0, 0, '2016-12-17 17:32:03'),
-(63, 'ahmet@noaktif.com', 'f31d009df71dd595e68a41b950d140c304b84980', 0x88992c391174a852f5ac5708aba96f3fbf3aba0d, 'ahmet no aktif', '1231231231', 1, 1, 0, '2016-12-17 17:32:26');
+(62, 'ahmet@aktif.com', 'e053b56abf7e056c09e296b1ac8556269ebf4f7b', 0x0a9ca6ed7aef5a68d81fffe36de7cb35813bc0a9, 'ahmet_aktif', '1231231231', 1, 0, 0, '2016-12-17 17:32:03'),
+(63, 'ahmet@noaktif.com', 'e053b56abf7e056c09e296b1ac8556269ebf4f7b', 0x0a9ca6ed7aef5a68d81fffe36de7cb35813bc0a9, 'ahmet no aktif', '1231231231', 1, 1, 0, '2016-12-17 17:32:26'),
+(65, 'ahmet@test.com', 'e053b56abf7e056c09e296b1ac8556269ebf4f7b', 0x0a9ca6ed7aef5a68d81fffe36de7cb35813bc0a9, 'ahmetahmet', '1231231231', 1, 1, 0, '2016-12-19 14:27:33'),
+(73, 'ahmetsafasezgin@gmail.com', 'e053b56abf7e056c09e296b1ac8556269ebf4f7b', 0x0a9ca6ed7aef5a68d81fffe36de7cb35813bc0a9, 'Ahmet Safa Sezgin', '5351231212', 1, 1, 0, '2016-12-22 18:14:50'),
+(75, 'asafasezgin@gmail.com', 'e053b56abf7e056c09e296b1ac8556269ebf4f7b', 0x0a9ca6ed7aef5a68d81fffe36de7cb35813bc0a9, 'Ahmet Safa Sezgin', '5351231212', 1, 1, 0, '2016-12-22 18:25:17'),
+(76, 'ozan_lahmacun@gmail.com', 'e053b56abf7e056c09e296b1ac8556269ebf4f7b', 0x0a9ca6ed7aef5a68d81fffe36de7cb35813bc0a9, 'lahmacuncu', '5351231212', 1, 1, 0, '2016-12-22 19:06:03'),
+(77, 'asdsad@sdasd.comasd', '242502413857e032b45362dbf1eeb4cce162a15b', 0x98596aeff0f7007bb8ffa158be5669e62391b402, 'asdsad', '2131231231', 0, 1, 0, '2016-12-22 19:50:55');
+
+-- --------------------------------------------------------
+
+--
+-- Görünüm yapısı `searchview`
+--
+DROP TABLE IF EXISTS `searchview`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `bookexchange`.`searchview`  AS  select `bookexchange`.`books`.`bookName` AS `bookName`,`bookexchange`.`books`.`author` AS `author`,`bookexchange`.`books`.`PublishYear` AS `PublishYear`,`bookexchange`.`users`.`fullname` AS `fullname`,`bookexchange`.`users`.`email` AS `email`,`bookexchange`.`users`.`phone` AS `phone` from (`bookexchange`.`books` join `bookexchange`.`users` on((`bookexchange`.`books`.`ownerId` = `bookexchange`.`users`.`id`))) ;
 
 --
 -- Dökümü yapılmış tablolar için indeksler
 --
+
+--
+-- Tablo için indeksler `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`bookId`);
 
 --
 -- Tablo için indeksler `users`
@@ -107,10 +182,15 @@ ALTER TABLE `users`
 --
 
 --
+-- Tablo için AUTO_INCREMENT değeri `books`
+--
+ALTER TABLE `books`
+  MODIFY `bookId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+--
 -- Tablo için AUTO_INCREMENT değeri `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
